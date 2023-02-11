3d8c460c6511775dccb9dcd2e1262cdc33b4afde
yuta-nishi <ypmtk437.0209@gmail.com>
2023-02-11 15:48:28 +0900

    add challenge2

---
+++ Challenge2/js/game.js
@@ -0,0 +1,71 @@
+let buttonColors = ["red", "blue", "yellow", "green"];
+let userClickedPattern = [];
+let gamePattern = [];
+let level = 0;
+let started = false;
+
+$(document).on("keydown", function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").on("click", function () {
+    userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        startOver();
+    }
+}
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100);
+    $("#" + randomChosenColor).fadeOut(100);
+    $("#" + randomChosenColor).fadeIn(100);
+    playSound(randomChosenColor);
+    level++;
+    $("#level-title").text("Level " + level);
+    userClickedPattern = [];
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(() => {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
